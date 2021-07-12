e8243255e2a06070ae7e9590a10c1fad8af591da
Mauricio Selfene <m.selfene@gmail.com>
2021-07-12 01:25:18 -0400

    Ingreso de conversor de monedas de dolar a peso y de peso a dolar

--- conversor.py
+++ conversor.py
@@ -1,3 +1,27 @@
-print("prueba")
+#Conversor de modenas
 
-##prueba asdas  asdas d
+#Dolar a peso chileno
+pesos = input("Cuantos pesos tienes? ")
+pesos = float(pesos)
+valor_dolar = 748
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+
+dolares = str(dolares)
+
+print("Tienes $" + dolares + " dolares")
+print("--------------------------------")
+
+
+
+#Peso Chileno a Dolar
+dolar = input("Cuantos dolares tienes ?")
+dolar = float(dolar)
+valor_peso = 0.0013
+pesos_dolar =  dolar / valor_peso
+pesos_dolar = round(pesos_dolar, 2)
+
+pesos_dolar = str(pesos_dolar)
+
+print("Tienes $" + pesos_dolar + " pesos Chileno")
+print("--------------------------------")
